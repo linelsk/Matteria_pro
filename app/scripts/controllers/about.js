7a29121a25d46ec60416e69b64449fd82db37fcc
linelsk <linelsk@hotmail.com>
2017-08-08 12:30:58 -0500

    inicio

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spraytecApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the spraytecApp
+ */
+angular.module('spraytecApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spraytecApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spraytecApp
+ */
+angular.module('spraytecApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
